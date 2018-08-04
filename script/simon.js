ee1de030feef32154c5baeb153a4a7d9d75d8c49
james-built <jgbuilt@gmail.com>
2018-08-04 22:14:11 +1200

    added function for random array generation

--- script/simon.js
+++ script/simon.js
@@ -1,13 +1,30 @@
-function startGame() {
-// add event listeners
-/* var lights = document.getElementsByClassName('light')
-for (let i = 0; i < lights.length; i++) {
-lights[i].addEventListener('click', playerInput())
+// global variables
+var playerArray = [];
+var computerArray = [];
+var level = 4;
 
-console.log("hello");
-}*/
+function startGame() {
+// get array for game
+getComputerArray();
+// play through array on board
+for (let i = 0; i < computerArray.length; i++) {
+  fireLight(i);
+}
+// listen for player input and compare
 }
 
 function playerInput(value) {
-  console.log(value);
+
+}
+
+function getComputerArray() {
+  var computerArray = [];
+  for (let i = 0; i < level; i++) {
+    computerArray.push((Math.floor(Math.random() * 8) + 1));
+  }
+  return computerArray;
+}
+
+function fireLight() {
+  
 }
