ee6e904bcbd0ebed0f22a56bb4cbf2e09be45d49
james-built <jgbuilt@gmail.com>
2018-08-05 00:11:36 +1200

    created a timer function to fire lights on a timer, started creating compare function & score + level iteration

--- script/simon.js
+++ script/simon.js
@@ -2,16 +2,33 @@
 var playerArray = [];
 var computerArray = [];
 var level = 4;
+var score = 0;
 
 function startGame() {
 // get array for game
 var currentArray = getComputerArray();
-// play through array on board
-for (let i = 0; i < currentArray.length; i++) {
-  fireLight(currentArray[i]);
+// play through array on board on timer
+function timedEvent(counter){
+  if(counter < currentArray.length){
+    setTimeout(function(){
+      fireLight(currentArray[counter]);
+      counter++;
+      timedEvent(counter);
+    }, 600);
+  }
 }
+timedEvent(0);
+
 // listen for player input and compare
 
+if (playerArray === computerArray) {
+score += 250;
+  if (score > 750) {level = 6}
+// play a win sound?
+setTimeout(startGame(), 2000);
+} else {
+  //lose events
+}
 }
 
 /* function playerInput(value) {
@@ -20,7 +37,7 @@ for (let i = 0; i < currentArray.length; i++) {
 
 function getComputerArray() {
   var computerArray = [];
-  for (let i = 0; i < level; i++) {
+  for (let i = 0; i < level; i++) { //uses the level provided to set the array length
     computerArray.push((Math.floor(Math.random() * 8) + 1));
   }
   console.log(computerArray);
