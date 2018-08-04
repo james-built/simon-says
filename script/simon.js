2bcfc80f45ba34fd6b3249f32cdf23802a23d6fa
james-built <jgbuilt@gmail.com>
2018-08-04 23:19:00 +1200

    added function to fire lights

--- script/simon.js
+++ script/simon.js
@@ -5,26 +5,30 @@ var level = 4;
 
 function startGame() {
 // get array for game
-getComputerArray();
+var currentArray = getComputerArray();
 // play through array on board
-for (let i = 0; i < computerArray.length; i++) {
-  fireLight(i);
+for (let i = 0; i < currentArray.length; i++) {
+  fireLight(currentArray[i]);
 }
 // listen for player input and compare
-}
-
-function playerInput(value) {
 
 }
+
+/* function playerInput(value) {
+
+} */
 
 function getComputerArray() {
   var computerArray = [];
   for (let i = 0; i < level; i++) {
     computerArray.push((Math.floor(Math.random() * 8) + 1));
   }
+  console.log(computerArray);
   return computerArray;
 }
 
-function fireLight() {
-  
+function fireLight(num) {
+  console.log(document.getElementById(num))
+  // set a css variable for the button to play animation
+  // reset the css variable to default state
 }
