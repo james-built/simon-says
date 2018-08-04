b3007a8b25302e4006ab561fe0395b4ad4da6ba4
james-built <jgbuilt@gmail.com>
2018-08-04 18:49:15 +1200

    added script file

---
+++ script/simon.js
@@ -0,0 +1,13 @@
+function startGame() {
+// add event listeners
+/* var lights = document.getElementsByClassName('light')
+for (let i = 0; i < lights.length; i++) {
+lights[i].addEventListener('click', playerInput())
+
+console.log("hello");
+}*/
+}
+
+function playerInput(value) {
+  console.log(value);
+}
